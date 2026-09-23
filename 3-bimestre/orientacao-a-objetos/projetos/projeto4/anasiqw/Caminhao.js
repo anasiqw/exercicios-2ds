ef317214592593437b85cb1f91a706a24b95f1b9
Ana Dávila <ana.siqueira57@aluno.ce.gov.br>
2026-09-23 14:16:19 -0300

    Delete 3-bimestre/orientacao-a-objetos/projetos/projeto4/anasiqw/Caminhao.js

--- 3-bimestre/orientacao-a-objetos/projetos/projeto4/anasiqw/Caminhao.js
+++
@@ -1,9 +0,0 @@
-const Veiculo = require("./Veiculo");
-
-class Caminhao extends Veiculo {
-    detalhes() {
-        return "Caminhão disponível para retirada";
-    }
-}
-
-module.exports = Caminhao;
